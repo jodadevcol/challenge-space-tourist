8634f7207f9d70017494a765e67e8616bae32941
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-09-12 22:28:56 +1000

    feat: add config of theme tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				home: "url(/backgrounds/mobile-home-bg.jpg)"
+			},
+			colors: {
+				"primary": "#0b0d17",
+				"secondary": "#d0d6f9"
+			},
+			fontFamily: {
+				sans: ['Barlow Condensed', 'sans-serif', ...defaultTheme.fontFamily.sans],
+				serif: ['Bellefair', 'serif', ...defaultTheme.fontFamily.serif],
+			}
+		},
 	},
 	plugins: [],
 }
